49e9f7fcb8bd9b60b6550e822655ec2b8b918a18
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-04-25 13:12:43 -0300

    imprime la data

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 function translate() {
-    var title = document.getElementById("form-signin-heading");
-    var email = document.getElementById("inputEmail");
-    var password = document.getElementById("inputPassword");
-    var remember = document.getElementsByTagName("span")[0];
-    var button = document.getElementsByClassName("btn")[0];
-
-    title.innerHTML = "Por favor inicia sesión";
-    email.placeholder = "Correo Electrónico";
-    password.placeholder = "Contraseña";
-    remember.innerHTML = "Recordar datos";
-    button.innerHTML = "Iniciar Sesión";
+    //Traducirá el título del formulario
+    var title = document.getElementById('form-signin-heading');
+    title.innerHTML = 'Por favor inicia sesión';
+    //Traducirá el texto del placeholder del primer input
+    var email = document.getElementById('inputEmail');
+    email.placeholder = 'Correo electrónico';
+    //Traduciráel texto del placeholder del primer input
+    var password = document.getElementById('inputPassword');
+    password.placeholder = 'Contraseña';
+    //Traducirá el texto junto al checkbox
+    var remember = document.getElementsByTagName('span')[0];
+    remember.innerHTML = 'Recordar datos';
+    //Traducirá el texto del botón
+    var button = document.getElementsByClassName('btn')[0];
+    button.innerHTML = 'Iniciar Sesión';
 }
 
 translate();
 
-var datos = [];
-forEach(function(valores) {
-    datos.push(valores.email)
-    console.log(email);
-});
+function mostrarData() {
+    var email = document.getElementById('inputEmail').value;
+    var pass = document.getElementById('inputPassword').value;
+    document.getElementById("printEmail").innerHTML = '<div><h4>Datos de formulario</h4></div>' +
+        '<div>El correo electrónico ingresado es:</div>' + email + '<div>La clave ingresada es:</div>';
+    document.getElementById("printPass").innerHTML = pass;
+
+}
